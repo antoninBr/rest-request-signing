d3a8c275436db6ce1dc1f5bfa532a20a54967106
antoninBr <antonin.brugnot@laposte.net>
2017-04-06 14:13:16 +0200

    End of Week Lunch Break Working on Timestamp Implementation

--- rest-request-signing-core/src/main/java/com/brugnot/security/core/timestamp/v1/KeystoreRestSignatureTimestampService.java
+++ rest-request-signing-core/src/main/java/com/brugnot/security/core/timestamp/v1/KeystoreRestSignatureTimestampService.java
@@ -1,17 +1,24 @@
 package com.brugnot.security.core.timestamp.v1;
 
 import com.brugnot.security.core.crypt.impl.AbstractCrypo;
+import com.brugnot.security.core.exception.crypt.CryptoComponentInstantiationException;
 import com.brugnot.security.core.exception.timestamp.RestSignatureTimestampException;
 import com.brugnot.security.core.timestamp.RestSignatureTimestampService;
 import com.brugnot.security.core.tools.KeystoreLoader;
+import com.brugnot.security.rest.commons.logging.DebugLogType;
+import com.brugnot.security.rest.commons.logging.LoggedItem;
+import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import javax.inject.Inject;
-import java.security.Key;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
+import java.security.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 /**
@@ -23,31 +30,162 @@ import java.util.Date;
 public class KeystoreRestSignatureTimestampService extends AbstractCrypo implements RestSignatureTimestampService {
 
     /**
-     * DateFormat to Use for timestamp
+     * DateFormat to use for timestamp
      */
     private final static SimpleDateFormat timestampDataFormat = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SSS");
 
+    /**
+     * LOGGER
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(KeystoreRestSignatureTimestampService.class);
+
+    /**
+     * Keystore Loader
+     */
     private KeystoreLoader keystoreLoader;
 
+    /**
+     * Key Alias inside Keystore
+     */
     private String keyAlias;
 
+    /**
+     * Key Password
+     */
     private String keyPassword;
+
+    /**
+     * Cipher Algorithm
+     */
+    private String cipherAlgorithm;
 
     @Override
     public String createTimestamp() throws RestSignatureTimestampException {
 
-        //TODO: use crypto methods to encode the created timestamp (using key in keystore)
         String currentTimestamp = getCurrentNormalizedTimestamp();
-        Key timestampSymmetricKey = getTimestampSymmetricKey();
 
-        return null;
+        LOGGER.debug(createItemDebugLog(DebugLogType.PROCESSING,"current Timestamp",LoggedItem.STRING,currentTimestamp));
+
+        String encryptedTimestampAsBase64 = Base64.encodeBase64String(encryptTimestamp(currentTimestamp));
+
+        LOGGER.debug(createItemDebugLog(DebugLogType.OUTPUT,"encrypted Timestamp As Base64", LoggedItem.STRING,encryptedTimestampAsBase64));
+
+        return encryptedTimestampAsBase64;
     }
 
     @Override
     public boolean validateTimestamp(String encodedTimestamp) throws RestSignatureTimestampException {
 
-        //TODO: use crypto methods to decode the created timestamp (using key in keystore)
+        LOGGER.debug(createItemDebugLog(DebugLogType.INPUT_ARGUMENT,"encoded as base64 encrypted Timestamp",LoggedItem.STRING,encodedTimestamp));
+
+        byte[] encryptedTimestampAsByteArray = Base64.decodeBase64(encodedTimestamp);
+
+        LOGGER.debug(createItemDebugLog(DebugLogType.PROCESSING,"encrypted Timestamp as Byte Array",LoggedItem.STRING,Arrays.toString(encryptedTimestampAsByteArray)));
+        String receivedTimestamp = decryptTimestamp(encryptedTimestampAsByteArray);
+
+        LOGGER.debug(createItemDebugLog(DebugLogType.PROCESSING,"received timestamp",LoggedItem.STRING,receivedTimestamp));
+
+        if(validateTimestampTowardsTime(receivedTimestamp)){
+            if(validateTimestampTowardsOccurrences(receivedTimestamp)){
+                return true;
+            }else {
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+
+    private boolean validateTimestampTowardsOccurrences(String receivedTimestamp) {
+        LOGGER.debug("Validating timestamp toward occurrences");
         return false;
+    }
+
+    private boolean validateTimestampTowardsTime(String receivedTimestamp) {
+        LOGGER.debug("Validating timestamp toward time");
+        return false;
+    }
+
+
+
+    /**
+     * Encrypt the provided timestamp (as String)
+     * @param currentTimestamp
+     * @return byte array
+     * @throws RestSignatureTimestampException
+     */
+    private byte[] encryptTimestamp(String currentTimestamp) throws RestSignatureTimestampException {
+        Key timestampSymmetricKey = getTimestampSymmetricKey();
+        Cipher timestampEncryptCipher;
+
+        try {
+            timestampEncryptCipher = createCipher(cipherAlgorithm);
+        } catch (CryptoComponentInstantiationException e) {
+            throw new RestSignatureTimestampException("Error while instantiating crypto components for timestamp encryption",e);
+        }
+
+        try {
+            timestampEncryptCipher.init(Cipher.ENCRYPT_MODE, timestampSymmetricKey);
+        } catch (InvalidKeyException e) {
+            throw new RestSignatureTimestampException("Error while initializing cipher for timestamp encryption",e);
+        }
+
+        byte[] encryptedTimestampByteArray;
+
+        try {
+            byte[] currentTimestampAsByteArray = currentTimestamp.getBytes();
+            LOGGER.debug(createItemDebugLog(DebugLogType.PROCESSING,"current Timestamp As Byte Array",LoggedItem.STRING, Arrays
+                    .toString(currentTimestampAsByteArray)));
+            encryptedTimestampByteArray = timestampEncryptCipher.doFinal(currentTimestampAsByteArray);
+            LOGGER.debug(createItemDebugLog(DebugLogType.PROCESSING,"encrypted current Timestamp As Byte Array",LoggedItem.STRING, Arrays
+                    .toString(encryptedTimestampByteArray)));
+        } catch (IllegalBlockSizeException e) {
+            throw new RestSignatureTimestampException("Error while crypt the timestamp",e);
+        } catch (BadPaddingException e) {
+            throw new RestSignatureTimestampException("Error while crypt the timestamp",e);
+        }
+
+        return encryptedTimestampByteArray;
+    }
+
+
+
+    /**
+     * Decrypt the provided crypted timestamp (as byte array)
+     * @param encryptedTimestampAsByteArray
+     * @return decrypted timestamp
+     */
+    private String decryptTimestamp(byte[] encryptedTimestampAsByteArray) throws RestSignatureTimestampException {
+
+        Key timestampSymmetricKey = getTimestampSymmetricKey();
+        Cipher timestampDecryptCipher;
+
+        try {
+            timestampDecryptCipher = createCipher(cipherAlgorithm);
+        } catch (CryptoComponentInstantiationException e) {
+            throw new RestSignatureTimestampException("Error while instantiating crypto components for timestamp decryption",e);
+        }
+
+        try {
+            timestampDecryptCipher.init(Cipher.DECRYPT_MODE, timestampSymmetricKey);
+        } catch (InvalidKeyException e) {
+            throw new RestSignatureTimestampException("Error while initializing cipher for timestamp decryption",e);
+        }
+
+        byte[] timestampAsByteArray;
+
+        try {
+            timestampAsByteArray = timestampDecryptCipher.doFinal(encryptedTimestampAsByteArray);
+            LOGGER.debug(createItemDebugLog(DebugLogType.PROCESSING,"current Timestamp As Byte Array",LoggedItem.STRING, Arrays
+                    .toString(timestampAsByteArray)));
+        } catch (IllegalBlockSizeException e) {
+            throw new RestSignatureTimestampException("Error while crypt the timestamp",e);
+        } catch (BadPaddingException e) {
+            throw new RestSignatureTimestampException("Error while crypt the timestamp",e);
+        }
+
+        return new String(timestampAsByteArray);
+
     }
 
     //TODO : Move this method to AbstractCrypto
@@ -56,11 +194,11 @@ public class KeystoreRestSignatureTimestampService extends AbstractCrypo impleme
         try {
             return keystoreLoader.getKeyStore().getKey(keyAlias,keyPassword.toCharArray());
         } catch (KeyStoreException e) {
-           throw new RestSignatureTimestampException("",e);
+           throw new RestSignatureTimestampException("Error while opening the configured keystore file",e);
         } catch (NoSuchAlgorithmException e) {
-            throw new RestSignatureTimestampException("",e);
+            throw new RestSignatureTimestampException("Wrong Algorithm on keystore key extraction",e);
         } catch (UnrecoverableKeyException e) {
-            throw new RestSignatureTimestampException("",e);
+            throw new RestSignatureTimestampException("Key can't be extracted from the configured keystore",e);
         }
     }
 
@@ -87,6 +225,30 @@ public class KeystoreRestSignatureTimestampService extends AbstractCrypo impleme
     @Inject
     public void setKeystoreLoader(KeystoreLoader keystoreLoader) {
         this.keystoreLoader = keystoreLoader;
+    }
+
+    protected String createItemDebugLog(DebugLogType debugLogType, String itemName, LoggedItem loggedItem, Object object){
+
+        StringBuilder debugLogBuilder = new StringBuilder();
+
+        debugLogBuilder.append(debugLogType.getLoggingPrefix()+"=");
+
+        debugLogBuilder.append(itemName);
+
+        if(loggedItem.isLogSize()){
+            debugLogBuilder.append(",size="+loggedItem.getObjectSize(object));
+
+        }
+
+        if(loggedItem.isLogValue()){
+
+            debugLogBuilder.append(",value="+object);
+
+        }
+
+        return debugLogBuilder.toString();
+
+
     }
 
 }
